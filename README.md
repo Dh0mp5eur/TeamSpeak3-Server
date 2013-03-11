5825e7253e97f444c927ab1a345ee5564c5f34e2
Dh0mp5eur <dhomps.florian@gmail.com>
2013-03-11 22:21:21 +0100

    First package version

---
+++ README.md
@@ -0,0 +1,6 @@
+TeamSpeak3-Server
+=================
+
+TeamSpeak3 Server package for Debian
+
+Beta version.
---
+++ package.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+dpkg-deb --build amd64 teamspeak3-server_amd64.deb
+dpkg-deb --build i386 teamspeak3-server_i386.deb
